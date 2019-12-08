73319469fd1eeb35f6b074facb805865ed88f93e
F74066307 <markchen565@gmail.com>
2019-12-08 11:44:02 +0800

    change return message

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 
 machine = TocMachine(
-    states=["user", "state1", "state2"],
+    states=["user", "state1", "state2","state3","state4","state5","state6"],
     transitions=[
         {
             "trigger": "advance",
@@ -51,6 +51,19 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
+textlist[
+    "在想些什麼嗎?"
+    "聽些歌吧\nhttps://www.youtube.com/playlist?list=PL1NeGg1woXqlISJkxjgwHKgB8LmR7tk92"
+    "看些影片嗎?\nhttps://www.youtube.com/playlist?list=PL1NeGg1woXqk0_YA5OJkJZoibyqyqsUE9"
+    "是否是累了呢"
+    "一時找不到的東西往往都在身邊，轉過頭去看看吧"
+    "覺得這世界無趣?正巧，我也是"
+    "正如同我無法給予你什麼一樣，你也無法給任何人什麼"
+    "悲傷吧，哀慟吧，那便是你活著的證明"
+    "那是一個美好的日子，花兒綻放著，鳥兒在鳴叫，在這樣的日子裡，一個像你一樣的孩子…\n就該在地獄里焚燒殆盡\nhttps://youtu.be/wDgQdr8ZkTw"
+]
+
+hologura="https://www.youtube.com/playlist?list=PL1NeGg1woXqmEelvJb_OFtVi8iYJgNF6D"
 
 @app.route("/callback", methods=["POST"])
 def callback():
@@ -72,8 +85,21 @@ def callback():
         if not isinstance(event.message, TextMessage):
             continue
 
+            if event.message.text.find("聽歌")!=-1:
+                text="https://www.youtube.com/playlist?list=PL1NeGg1woXqlISJkxjgwHKgB8LmR7tk92"
+            elif event.message.text.find("ホロぐら")!=-1 or event.message.text.find("hologura")!=-1:
+                text=hologura
+            elif event.message.text.find("剪輯")!=-1 or event.message.text.find("切り抜き")!=-1:
+                text="https://www.youtube.com/playlist?list=PL1NeGg1woXqk0_YA5OJkJZoibyqyqsUE9"
+            elif event.message.text.find("ASMR")!=-1:
+                text="https://www.youtube.com/playlist?list=PL1NeGg1woXqlNFSy_AW3x6RwTqP5rTM_c"
+            elif event.message.text.find("mio")!=-1 or event.message.text.find("ookami")!=-1 or event.message.text.find("大神")!=-1 or event.message.text.find("ミオ")!=-1:
+                text="https://www.youtube.com/channel/UCp-5t9SrOQwXMU7iIjQfARg"
+            else :
+                text=random.choice(textlist)
+　　　　　　
         line_bot_api.reply_message(
-            event.reply_token, TextSendMessage(text=event.message.text)
+            event.reply_token, TextSendMessage(text)
         )
 
     return "OK"
